02574c4a12855c994e6dfbdc5c36c525da19ae39
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2026-09-17 13:57:24 -0300

    Update script.js

--- Projeto 2 - Gerenciador de Tarefas/script.js
+++ Projeto 2 - Gerenciador de Tarefas/script.js
@@ -3,17 +3,24 @@ const listaTarefas = []
 function cadastrarTarefa(e){
     e.preventDefault()
 
+    // 1. Coletar Dados
     const titulo = document.getElementById("titulo").value
 
+    // 2. Estruturar os dados (compilar/montar)
     const novaTarefa = {
         "titulo": titulo,
         "estado": "pendente"
     }
 
+    // 3. Armazenar os dados 
     listaTarefas.push(novaTarefa)
 
-    console.log(listaTarefas)
+    alert("Tarefa Cadastrada com Sucesso!")
     
-
     document.getElementById("form-cadastro").reset()
 }
+
+// Construir uma função chamada renderizarTarefas
+// 1. Ler a lista de tarefas
+// 2. Criar um novo li para cada tarefa
+// 3. Adicionar os li criados em um elemento(ul) chamado lista de tarefas
